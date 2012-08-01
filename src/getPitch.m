140e06a516039d95c317feb6920cd63f9e0ad481
Hermes Lyu <a108210@gmail.com>
2012-08-01 17:12:49 +0800

    Removed some files

--- src/getPitch.m
+++
@@ -1,6 +0,0 @@
-%get the pitch  notes in phrases
-function pitch= getPitch(scores);
-   for i = 1: length(scores)
-      pitch{i} = scores{i}(:, 4); %unit: % /100
-   end
-end
--- src/getScoreDuration.m
+++
@@ -1,6 +0,0 @@
-%get the nhhotes in phrases
-function dur = getScoreDuration(scores);
-   for i = 1: length(scores)
-      dur{i} = scores{i}(:, 2); %unit: beats
-   end
-end
